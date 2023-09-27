61fd3bd13924882d8af85d6345736d77ed4ee296
ronald-barcotech <146149932+ronald-barcotech@users.noreply.github.com>
2023-09-27 10:28:24 +0800

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,8 +24,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "aa76f29e38da8f8875ed91d01df30c00
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "55305bb114fff3141b296afa7b5b79df",
-"/": "55305bb114fff3141b296afa7b5b79df",
+"index.html": "224a5b230e97314cec89d2ebd6f29781",
+"/": "224a5b230e97314cec89d2ebd6f29781",
 "main.dart.js": "458a8725e0f2090e78b371e603c878e3",
 "manifest.json": "ad9061773b1103ab69acac2297d873c3",
 "version.json": "720d3650e4c01b7dc85e851433b9c58b"};
